cd93623756fb39c47d79d5f59cebe19e9b0e9cf5
MariceAshtiani <marice95@hotmail.no>
2022-09-29 21:04:16 +0200

    fixing home page

--- js/home.js
+++ js/home.js
@@ -1,3 +1,14 @@
 const images = document.querySelectorAll(".image-trigger img");
 const modal = document.querySelector(".home-modal");
 const modalImg = document.querySelector(".homeModalImg");
+
+images.forEach((image) => {
+    image.addEventListener("click", () => {
+        modalImg.src = image.src;
+        modal.classList.add("appear");
+
+    window.addEventListener("click", () => {
+        modal.classList.remove("appear");
+    });
+    });
+});
